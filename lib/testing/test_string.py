f9f6b6bdae14eaf2a2df3d2271105df7a23bfc0f
Megan <megan.nyakina@student.moringaschool.com>
2024-08-26 23:53:23 +0300

    completes lab

--- lib/testing/test_string.py
+++ lib/testing/test_string.py
@@ -6,6 +6,6 @@ def test_return_string():
     '''in string_functions, function "return_string()" returns a variable of type str.'''
     assert type(return_string()) == str
 
-def test_interpolate_string():
+def test_interapolate_string():
     '''in string_functions, function "interpolate_string()" takes a string and inserts it into another string.'''
     assert interpolate_string('Guido') == 'Hello, Guido!'
